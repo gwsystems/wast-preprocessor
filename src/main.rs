70913b80f598a89671d58d07b37ad52881c03621
Aisha Mohammed <amohammed6@gwu.edu>
2022-02-23 11:55:25 -0500

    Init

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use anyhow::Result;
+use std::env;
+use wasmparser::{Parser, Payload};
+
+fn main() -> Result<()> {
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        println!("Usage: {} in.wasm", args[0]);
+        return Ok(());
+    }
+
+    let buf: Vec<u8> = std::fs::read(&args[1])?;
+    for payload in Parser::new(0).parse_all(&buf) {
+        match payload? {
+            Payload::Version { .. } => {
+                println!("====== Module");
+            }
+            Payload::ExportSection(s) => {
+                for export in s {
+                    let export = export?;
+                    println!("  Export {} {:?}", export.field, export.kind);
+                }
+            }
+            Payload::ImportSection(s) => {
+                for import in s {
+                    let import = import?;
+                    println!("  Import {}::{}", import.module, import.field.unwrap());
+                }
+            }
+            _other => {
+                // println!("found payload {:?}", _other);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// use wasmparser::ExportSectionReader;
+
+// fn main() {
+//     println!("Entered");
+//     let mut export_reader = ExportSectionReader::new(data, 0).unwrap();
+//     for _ in 0..export_reader.get_count() {
+//         let export = export_reader.read().expect("export");
+//         println!("Export: {:?}", export);
+//     }
+// }
